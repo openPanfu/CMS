00d25d7b97465c085d3315d459a8ed68708fc9b6
Altro50 <altro50@msn.com>
2018-08-16 17:31:14 +0200

    Made /play generate a new ticket_id, since the gameserver now nulls it

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+use App\User;
+
 Route::get('/play', function () {
+	// Is the user logged in on the website?
+	if(Auth::Check()) {
+		// We get the user from the database.
+		$user_id = Auth::user()->id;
+		$current_user = User::find($user_id);
+
+		// We generate a new ticket_id for the user.
+		$newTicketId = uniqid("OPS_");
+		$current_user->ticket_id = $newTicketId;
+
+		// We store this new ticket in the databse and local auth.
+		$current_user->save();
+		Auth::user()->ticket_id = $newTicketId;
+	}
    return view('play');
 })->name('play');
