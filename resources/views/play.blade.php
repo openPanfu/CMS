6691133d11cae68e2105afd607d108438bb755f9
altro50 <altro50@msn.com>
2018-12-31 17:35:06 +0100

    Fix for linux PHP

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <div class="container text-center">
         @auth
-            <object base="/flashclient/" height="480" width="772" data="/flashclient/Panfu.swf?iServer=http://localhost:8000/InformationServer/&langId=EN&mode=dev&user={{Auth::user()->name}}&sessionKey={{Auth::user()->ticket_id}}"></object>
+            <object base="/FlashClient/" height="480" width="772" data="/FlashClient/Panfu.swf?iServer=http://localhost:8000/InformationServer/&langId=EN&mode=dev&user={{Auth::user()->name}}&sessionKey={{Auth::user()->ticket_id}}"></object>
         @endauth
         @guest
-             <object base="/flashclient/" height="480" width="772" data="/flashclient/Panfu.swf?iServer=http://localhost:8000/InformationServer/&langId=EN&mode=dev"></object>
+             <object base="/FlashClient/" height="480" width="772" data="/FlashClient/Panfu.swf?iServer=http://localhost:8000/InformationServer/&langId=EN&mode=dev"></object>
         @endguest
     </div>
 @endsection
